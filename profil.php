4a554acd520ead9a0b728ec7312f59bef5b5eaa7
ramez-brh <ramezbarhoumi702@gmail.com>
2026-03-02 11:00:19 +0100

    TP0 : Initialisation du projet

---
+++ profil.php
@@ -0,0 +1,20 @@
+<?php
+$nom = "AYARI";
+$prenom = "Asma";
+$email = "asma.ayari@email.com";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Profil utilisateur</title>
+</head>
+<body>
+    <h1>Profil utilisateur</h1>
+    
+    <p><strong>Nom :</strong> <?= $nom ?></p>
+    <p><strong>Prénom :</strong> <?= $prenom ?></p>
+    <p><strong>Email :</strong> <?= $email ?></p>
+    
+</body>
+</html>
